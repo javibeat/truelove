fd28ac0902ce9c1f093a28a1549265fc001f74c6
javibeat <javibeat@gmail.com>
2023-06-23 20:32:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,76 +3,68 @@ document.addEventListener("DOMContentLoaded", function() {
   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
   const logo = document.querySelector(".logo");
   const navMenu = document.querySelector(".nav-menu");
+  const portfolioButtons = document.querySelectorAll(".portfolio-menu-btn");
+  const portfolioItems = document.querySelectorAll(".portfolio-item");
+  const portfolioMenuBtns = document.querySelectorAll(".portfolio-menu-btn");
 
-  const adjustSectionsHeight = () => {
+  function adjustSectionsHeight() {
     const windowHeight = window.innerHeight;
 
     sections.forEach((section) => {
       section.style.height = `${windowHeight}px`;
     });
-  };
+  }
 
-  const centerSectionInView = () => {
+  function centerSectionInView() {
     const scrollPosition = window.scrollY;
     const windowHeight = window.innerHeight;
 
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
       const sectionHeight = section.offsetHeight;
-      const sectionOffset = 100;
+      const isSectionInView = scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight - windowHeight + 100;
 
-      if (
-        scrollPosition >= sectionTop &&
-        scrollPosition < sectionTop + sectionHeight - windowHeight + sectionOffset
-      ) {
-        section.classList.add("centered");
+      section.classList.toggle("centered", isSectionInView);
+
+      if (isSectionInView) {
         activateMenuLink(section.id);
-      } else {
-        section.classList.remove("centered");
       }
     });
 
     scrollToTopBtn.style.display = scrollPosition >= windowHeight / 2 ? "block" : "none";
-  };
+  }
 
-  const scrollToTop = () => {
+  function scrollToTop() {
     const homeSection = document.getElementById("home");
     homeSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
+  }
 
-  const activateMenuLink = (sectionId) => {
+  function activateMenuLink(sectionId) {
     const menuLinks = navMenu.querySelectorAll("a");
 
     menuLinks.forEach((link) => {
-      link.classList.toggle("active", link.getAttribute("href") === `#${sectionId}`);
+      const isActive = link.getAttribute("href") === `#${sectionId}`;
+      link.classList.toggle("active", isActive);
     });
-  };
+  }
 
-  logo.addEventListener("click", scrollToTop);
-  scrollToTopBtn.addEventListener("click", scrollToTop);
-
-  window.addEventListener("resize", adjustSectionsHeight);
-  window.addEventListener("scroll", centerSectionInView);
-
-  adjustSectionsHeight();
-  centerSectionInView();
-
-  // Smooth Scroll
-  const menuItems = document.querySelectorAll(".menu-item");
-
-  menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetSectionId = menuItem.getAttribute("href").substring(1);
-      const targetSection = document.getElementById(targetSectionId);
-      targetSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  function filterPortfolioItems(category) {
+    portfolioItems.forEach((item) => {
+      const itemCategories = item.getAttribute("data-category").split(" ");
+      const isActive = category === "all" || itemCategories.includes(category);
+      item.classList.toggle("active", isActive);
     });
-  });
+  }
 
-  // Scroll with Mouse Wheel
-  document.addEventListener("wheel", function(event) {
+  function handleMenuItemClick(e) {
+    e.preventDefault();
+    const targetSectionId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetSectionId);
+    targetSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  }
+
+  function handleWheelScroll(event) {
     event.preventDefault();
-
     const deltaY = event.deltaY;
     const windowHeight = window.innerHeight;
     const currentSection = document.querySelector(".centered");
@@ -81,36 +73,53 @@ document.addEventListener("DOMContentLoaded", function() {
     if (nextSection) {
       nextSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+  }
+
+  function toggleMobileMenu() {
+    navMenu.classList.toggle("active");
+  }
+
+  function handlePortfolioButtonClick() {
+    const category = this.getAttribute("data-filter");
+
+    portfolioMenuBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    this.classList.add("active");
+
+    portfolioItems.forEach((item) => {
+      const itemCategory = item.getAttribute("data-category");
+      const isActive = category === "all" || category === itemCategory;
+      item.style.display = isActive ? "block" : "none";
+    });
+  }
+
+  logo.addEventListener("click", scrollToTop);
+  scrollToTopBtn.addEventListener("click", scrollToTop);
+  window.addEventListener("resize", adjustSectionsHeight);
+  window.addEventListener("scroll", centerSectionInView);
+
+  portfolioMenuBtns.forEach((btn) => {
+    btn.addEventListener("click", handlePortfolioButtonClick);
   });
 
-  // Mobile Menu
-  const toggleMobileMenu = () => {
-    navMenu.classList.toggle("active");
-  };
+  // Smooth Scroll
+  const menuItems = document.querySelectorAll(".menu-item");
 
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", handleMenuItemClick);
+  });
+
+  // Scroll with Mouse Wheel
+  document.addEventListener("wheel", handleWheelScroll);
+
+  // Mobile Menu
   const menuToggle = document.querySelector(".menu-toggle");
   menuToggle.addEventListener("click", toggleMobileMenu);
 
-  // GALERÍA PORTFOLIO
-  const menuButtons = document.querySelectorAll(".portfolio-menu-btn");
-  const galleryItems = Array.from(document.querySelectorAll(".portfolio-item"));
+  // Initial filtering (show all items)
+  handlePortfolioButtonClick.call(portfolioMenuBtns[0]);
 
-  menuButtons.forEach((button) => {
-    button.addEventListener("click", function() {
-      const filter = button.getAttribute("data-filter");
-
-      menuButtons.forEach((btn) => btn.classList.remove("active"));
-      button.classList.add("active");
-
-      galleryItems.forEach((item) => {
-        const category = item.getAttribute("data-category");
-
-        if (filter === "all" || category === filter) {
-          item.style.display = "block";
-        } else {
-          item.style.display = "none";
-        }
-      });
-    });
-  });
+  adjustSectionsHeight();
+  centerSectionInView();
 });
